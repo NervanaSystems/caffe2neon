1e7a59726f01d168f2f213e67079852ea53f3cae
Evren Tumer <evren@nervanasys.com>
2016-02-08 14:08:04 -0800

    added copyright messages

--- decaffeinate.py
+++ decaffeinate.py
@@ -1,5 +1,19 @@
 # !/usr/bin/env python
-#####################################################################
+# ----------------------------------------------------------------------------
+# Copyright 2014 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+#
 # Convert Caffe model to neon format.
 # 
 # First the caffe prototxt file and model are loaded up and
@@ -12,7 +26,6 @@
 # 
 # To run this caffe must be on the python path
 #
-#####################################################################
 
 import os
 import pickle
